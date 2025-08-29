1a29d06f8fde269b90f7d670006c21a6e75d0dd5
William Ferreira <55250304+tecwilliam38@users.noreply.github.com>
2025-08-29 12:10:08 -0300

    limpo

--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -3,52 +3,63 @@ import { feedStyle } from './styles.js';
 import { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
 import { AuthContext } from '../../context/auth.js';
+import { supabase } from '../../api/supabaseClient.js';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
-export default function FeedScreen() {
-    const { user } = useContext(AuthContext);
+export default function FeedScreen({ user, id_user }) {
     const { itemBody, title, container, listContainer, itemImage, titleDescription, titleEpisodios, titleRank, itemDescription } = feedStyle;
 
+
+    const [userImages, setUserImages] = useState([]);
     const [animes, setAnimes] = useState([]);
+
     useEffect(() => {
-        axios.get('https://api.jikan.moe/v4/top/anime?page=1&limit=10')
-            .then(response => setAnimes(response.data.data))
-
-
-            .catch(error => console.error());
+        fetchUserImages();
     }, []);
-    const renderItem = ({ item }) => (
-        <View style={itemBody}>
-            <Text style={title}>{item.title}</Text>
-            <Image source={{ uri: item.images.jpg.image_url }} style={itemImage} />
-            {/* <Text style={title}>{item.synopsis}</Text> */}
-            <View style={titleDescription}>
-                <Text style={titleEpisodios}>Episódios: {item.episodes}</Text>
-                <Text style={titleRank}>Ranking:{item.rank}</Text>
+
+    const fetchUserImages = async () => {
+        try {
+            const { data, error } = await supabase
+                .from('anama_posts')
+                .select('image_url')
+                .eq('id_user', id_user)
+                .order('created_at', { ascending: false });
+
+            if (error) throw error;
+            // Atualiza o estado com as URLs das imagens
+            // console.log("Imagens recebidas:", data);
+            setUserImages(data.map(item => item.image_url));
+        } catch (err) {
+            console.error('Erro ao buscar imagens:', err.message);
+        }
+    };
+
+    const renderItem = ({ item }) => {
+        return (
+            <View style={itemBody}>
+                <Image
+                    source={{ uri: item }}
+                    style={itemImage}
+                    resizeMode='cover'
+                />
             </View>
-            <Text numberOfLines={10} ellipsizeMode="tail" style={itemDescription}>Ranking:{item.synopsis}</Text>
-        </View>
-    );
+        )
+    };
 
     return (
         <View style={container}>
-            {/* <View style={{ marginTop: 30 }}>
-                <Text>
-                    {user.user_email}
-                </Text>
-
-            </View> */}
-
+            {/* <View style={{ marginTop: 30 }}><Text>{user.user_email}</Text></View> */}
             <FlatList
-                data={animes}
+                data={userImages}
                 renderItem={renderItem}
-                keyExtractor={item => item.mal_id.toString()}
+                keyExtractor={(item, index) => index.toString()}
                 showsHorizontalScrollIndicator={false}
                 contentContainerStyle={listContainer}
             />
         </View>
     )
 }
+
 
