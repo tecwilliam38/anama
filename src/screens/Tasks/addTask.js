56b27c410b951c4732678863cc64fb01492ab980
William Ferreira <55250304+tecwilliam38@users.noreply.github.com>
2025-09-29 09:18:41 -0300

    funciona

--- src/screens/Tasks/addTask.js
+++ src/screens/Tasks/addTask.js
@@ -20,6 +20,7 @@ export default function AddTask() {
     const [status, setStatus] = useState("Status");
 
     const [idUser, setIdUser] = useState("");
+    const [price, setPrice] = useState("");
     const [idTecnico, setIdTecnico] = useState();
     const [idService, setIdService] = useState("");
     const [bookingDate, setBookingDate] = useState("");
@@ -28,6 +29,43 @@ export default function AddTask() {
     useEffect(() => {
         LoadClients();
     }, []);
+
+    // console.log("Clientes:", clients.map(c => c.id_client));
+    // console.log("Primeiro cliente:", clients[0]);
+    // console.log("ID do primeiro cliente:", clients[0]?.id_client);
+
+    const token = user.token;
+
+    const agendaData = {
+        id_service: idService,
+        id_client: idClients,
+        price,
+        status,
+        booking_datetime: '2025-10-10T14:00:00'
+    };
+
+
+    const insertAgenda = async (token, agendaData) => {
+        try {
+            const response = await api.post(
+                '/client/agendamentos/add', // substitua pelo IP e porta do seu backend
+                agendaData,
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                        'Content-Type': 'application/json',
+                    },
+                }
+            );
+
+            console.log('ID do agendamento:', response.data.id_appointment);
+            return response.data.id_appointment;
+        } catch (error) {
+            console.error('Erro ao inserir agendamento:', error.response?.data || error.message);
+            return null;
+        }
+    };
+
 
     async function LoadClients() {
         try {
@@ -53,11 +91,15 @@ export default function AddTask() {
     return (
         <View style={styles.card}>
             <View style={styles.formRow}>
-                <TextInput style={styles.input} placeholder="Número do Chamado" keyboardType="numeric" />
+                <TextInput
+                    value={idService}
+                    onChangeText={setIdService}
+                    style={styles.input}
+                    placeholder="Número do Chamado" keyboardType="numeric" />
                 {/* <Text style={styles.label}>Clientes</Text> */}
                 <Picker
-                    selectedValue={clients}
-                    onValueChange={(value) => setClients(value)}
+                    selectedValue={idClients}
+                    onValueChange={(value) => setIdClients(value)}
                     style={styles.picker}
                 >
                     <Picker.Item label="Cliente" value="" />
@@ -69,7 +111,11 @@ export default function AddTask() {
             <View style={styles.formRow}>
                 <View style={styles.chamado}>
                     <Text style={styles.prefixo}>R$</Text>
-                    <TextInput style={styles.input} placeholder="Valor do Chamado" keyboardType="numeric" />
+                    <TextInput
+                        value={price}
+                        onChangeText={setPrice}
+                        style={styles.input}
+                        placeholder="Valor do Chamado" keyboardType="numeric" />
                 </View>
                 <Picker
                     label="Status"
